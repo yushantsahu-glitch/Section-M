1694e7489a2fddd0db491f4f7c4fe84a491066f6
Yushant  Sahu <yushantsahu@gmail.com>
2026-06-01 09:23:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://yushant:12345@clusterm.oqwr8oa.mongodb.net/");
+
+const User = mongoose.model("User", {
+  name: String
+});
+
+app.post("/users", async (req, res) => {
+  const user = new User({ name: req.body.name });
+  await user.save();
+  res.send(user);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000");
+});
